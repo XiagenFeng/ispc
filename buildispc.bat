34b976916d0da6e479ac2f035b832bd8fea75ef8
Chen Wenli <chenwenli@chenwenli.com>
2017-10-10 16:35:03 +0800

    use in-tree llvm and clang to build'

--- buildispc.bat
+++ buildispc.bat
@@ -2,10 +2,11 @@
 
 REM If LLVM_INSTALL_DIR isn't set globally in your environment,
 REM it can be set here_
-REM set LLVM_INSTALL_DIR=c:\users\mmp\llvm-dev
-REM set LLVM_VERSION=LLVM_3_2
+set LLVM_INSTALL_DIR=..\..\
+set LLVM_VERSION=LLVM_6_0
 
 REM Both the LLVM binaries and python need to be in the path
-set path=%LLVM_INSTALL_DIR%\bin;%PATH%;c:\cygwin\bin
+set path=%LLVM_INSTALL_DIR%\bin;%PATH%
 
-msbuild ispc.vcxproj /V:m /p:Platform=Win32 /p:Configuration=Release
+msbuild ispc.vcxproj /V:m /p:Platform=Win32 /p:Configuration=Release /t:rebuild
+
