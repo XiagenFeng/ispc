ff28b25ab5c95add92a34e09f4f340b9b1a68648
Chen Wenli <chenwenli@chenwenli.com>
2018-03-16 11:44:34 +0800

    build with LLVM/Clang 7.0

--- buildispc.sh
+++ buildispc.sh
@@ -4,7 +4,7 @@
 # it can be set here_
 LLVM_INSTALL_DIR=../../`uname -s`/
 LLVM_HOME=$LLVM_INSTALL_DIR
-LLVM_VERSION=LLVM_5_0
+LLVM_VERSION=LLVM_7_0
 
 # Both the LLVM binaries and python need to be in the path
 PATH=$LLVM_INSTALL_DIR/bin:$PATH
