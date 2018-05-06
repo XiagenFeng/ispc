6858dc4e752be2ac1d8b65de06d6fb661232c0c5
Chen Wenli <wenli_chen03@hotmail.com>
2018-05-06 12:25:47 +0800

    fix windows build error

--- buildispc.bat
+++ buildispc.bat
@@ -3,7 +3,7 @@
 REM If LLVM_INSTALL_DIR isn't set globally in your environment,
 REM it can be set here_
 set LLVM_INSTALL_DIR=..\..\Windows\
-set LLVM_VERSION=LLVM_6_0
+set LLVM_VERSION=LLVM_7_0
 
 REM Both the LLVM binaries and python need to be in the path
 set path=%LLVM_INSTALL_DIR%\bin;%PATH%
---
+++ quick_fix.cpp
@@ -0,0 +1,8 @@
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Value.h"
+
+namespace llvm {
+    void Module::dump() const {};
+    void Value::dump() const {};
+}
+
